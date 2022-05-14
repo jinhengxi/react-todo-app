26b9b522381aa4202e00a44d798f759ee167c9b8
Hengxi <jinhengxi93@gmail.com>
2022-05-14 20:39:07 +0900

    modify 컴포넌트 기능 최적화 중...

--- src/components/TodoListItem.js
+++ src/components/TodoListItem.js
@@ -1,20 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { MdCheckBoxOutlineBlank, MdCheckBox, MdRemoveCircleOutline, } from 'react-icons/md';
 import './TodoListItem.scss'
 import cn from 'classnames';
 
-const TodoListItem = ( { todo, onRemove, onToggle } )=>{
+const TodoListItem = ( {todo , onRemove, onToggle} )=>{
 
-    const { id, text, checked } = todo;
+    const { id, text, checked } = todo
 
     return(
         <div className='TodoListItem'>
-            <div className={cn('checkbox', {checked})} onClick={()=>onToggle(id)}>
-            {
-            checked
-            ?<MdCheckBox/>
-            :<MdCheckBoxOutlineBlank/>
-            }
+            <div className={cn('checkbox',{checked})} onClick={()=>onToggle(id)}>
+                {
+                checked
+                ?<MdCheckBox/>
+                :<MdCheckBoxOutlineBlank/>
+                }
                 <div className='text'>{text}</div>
             </div>
             <div className='remove' onClick={()=>onRemove(id)}>
@@ -24,4 +24,4 @@ const TodoListItem = ( { todo, onRemove, onToggle } )=>{
     )
 }
 
-export default TodoListItem;
+export default React.memo(TodoListItem);
